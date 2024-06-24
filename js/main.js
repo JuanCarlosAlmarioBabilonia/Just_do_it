ab873f65b8219e1db5ca10334e76355b4289b30a
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-23 19:17:28 -0500

    feat: :lipstick: Apariciion de la data en el html, y funcionalidades basicas pero no terminadas

--- js/main.js
+++ js/main.js
@@ -8,3 +8,29 @@ addEventListener("DOMContentLoaded", async () => {
     tareas.innerHTML = await tasksSection(data);
 })
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tareas = document.querySelector('.tareas_main');
+    tareas.addEventListener('click', (e) => {
+        if (e.target.classList.contains('check')) {
+            const tarea = e.target.closest('.tareas');
+            if (tarea) {
+                const nuevaTarea = document.createElement('section');
+                nuevaTarea.className = 'tareas2';
+                nuevaTarea.innerHTML = `
+                    <p><del>${tarea.querySelector('p').textContent}</del></p>
+                    <img class="check" src="/storage/imgs/check.png">
+                    <img class="trash" src="/storage/imgs/trash.png">
+                `; 
+                tarea.replaceWith(nuevaTarea); 
+            }
+        } else if (e.target.classList.contains('trash')) {
+            const tarea = e.target.closest('.tareas2');
+            if (tarea) {
+                tarea.remove();
+            }
+        }
+    });
+});
+
+
--- js/modules/data.js
+++ js/modules/data.js
@@ -9,4 +9,37 @@ export const getAllTasks = async () => {
     return data;
 }
 
+export const addTask = async (tasks) => {
+    const url = ("https://66778b0c145714a1bd74fd44.mockapi.io/Todolist");
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(tasks)
+    };
+    let res = await fetch(url, options);
+    let data = await res.json();
+    return data
+}
 
+const addTaskButton = document.querySelector("#addTaskButton");
+addTaskButton.addEventListener("click", async () => {
+    const task = document.querySelector('#input__search').value;
+    const status = "ready";  
+    
+    const taskData = {
+        task: task,
+        status: status
+    };
+
+    try {
+        const data = await addTask(taskData);
+        console.log('Success:', data);
+        alert('Tarea subida exitosamente');
+        document.querySelector('#input__search').value = '';
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al subir la tarea');
+    }
+})

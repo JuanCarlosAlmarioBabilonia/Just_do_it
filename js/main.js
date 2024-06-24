91e0f519f6d74c302ba7d8ff2d51b758b458c1ec
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-06-24 08:59:36 -0500

    feat: :construction: Desarrollo parcial del delete y put

--- js/main.js
+++ js/main.js
@@ -33,4 +33,48 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+export const deleteTask = async ({id}) => {
+    const url = `https://66778b0c145714a1bd74fd44.mockapi.io/Todolist/${id}`;
+    const options = {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    let res = await fetch(url, options);
+    if(res.status == 404) return {status: 204, message: `La tarea ${id} NO fue encontrada en la base de datos`}
+    let data = await res.json();
+    data.status = 202
+    data.message = `La tarea ${id} fue eliminada correctamente de la base de datos`
+    return data;
+}
+
+// console.log(await deleteTask({id: "52"}))
+
+
+export const updateTask = async (task) => {
+    const url = `https://66778b0c145714a1bd74fd44.mockapi.io/Todolist/${task.id}`;
+    const options = {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    };
+    let res = await fetch(url, options);
+    console.log(res)
+    if (res.status === 404) {
+        return {
+            status: 404,
+            message: `La tarea ${task.id} no fue encontrada en la base de datos`
+        };
+    }
+    let data = await res.json();
+    data.status = 202;
+    data.message = `La tarea ${task.id} fue actualizada correctamente en la base de datos`;
+    return data;
+};
+
+console.log(await updateTask({id: "56", task: "hi"}))
+
 
--- js/modules/data.js
+++ js/modules/data.js
@@ -43,3 +43,38 @@ addTaskButton.addEventListener("click", async () => {
         alert('Error al subir la tarea');
     }
 })
+
+export const deleteTask = async (id) => {
+    const url = `https://66778b0c145714a1bd74fd44.mockapi.io/Todolist/${id}`;
+    const options = {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    let res = await fetch(url, options);
+    console.log('Respuesta del servidor:', res);
+    let data = await res.json();
+    console.log('Datos de la respuesta:', data);
+    return data;
+}
+
+const tareasContainer = document.querySelector('.tareas2_main');
+
+tareasContainer.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('trash')) {
+        const tarea = event.target.closest('.tareas2');
+        console.log(tarea)
+        if (tarea) {
+            const taskId = tarea.dataset.taskId;
+            try {
+                await deleteTask(taskId);
+                console.log('Tarea eliminada exitosamente');
+                tarea.remove();
+            } catch (error) {
+                console.error('Error al eliminar la tarea:', error);
+                alert('Error al eliminar la tarea');
+            }
+        }
+    }
+});
